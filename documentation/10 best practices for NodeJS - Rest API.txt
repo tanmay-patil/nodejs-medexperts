7f059895ab4389905ccce216c24cd28aae360343
Tanmay Patil <tanmayrmfc@gmail.com>
2018-09-27 21:25:50 +0530

    updated documentation

---
+++ documentation/10 best practices for NodeJS - Rest API.txt
@@ -0,0 +1,1 @@
+https://blog.risingstack.com/10-best-practices-for-writing-node-js-rest-apis/
--- server.js
+++ server.js
@@ -1,1 +1,5 @@
 let db = require('./config/database/db_config');
+
+var app = require('express')();
+var someController = require('./webservices/user_account/user_account');
+app.use('/test2', someController);
